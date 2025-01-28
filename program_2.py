fd26598e41f76130783cfe78fa2faaf9db819941
FriedCoconut64 <vansterapples@gmail.com>
2025-01-28 09:25:25 -0600

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,13 +1,18 @@
+print("Type the ages of Five Friends")
 
 def average_age():
-    # Get User Input
+    print("Type the ages of Five Friends")
+age_1 = int(input("1st:"))
+age_2 = int(input("2nd:"))
+age_3 =int(input("3rd:"))
+age_4 =int(input("4th:"))
+age_5 =int(input("5th:"))
 
+total = (age_1 + age_2 + age_3 + age_4 + age_5)
 
-    # Sum ages
+avg_age = total/5
 
-    # Average the ages
+print(avg_age)
 
-    # Print the results
-
-# Line which calls the above function.
 average_age()
+#Program #2, Donovan Thompson 1/27/25
